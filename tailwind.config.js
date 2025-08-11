e7b7afb431392f654a4def66be3fea767788c382
Jack Koppa <hello@jackkoppa.com>
2025-08-11 18:43:57 -0400

    Theme: baby blue, lilac, and cream color scheme; update brand colors and icon badges

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,17 +12,17 @@ module.exports = {
           100: '#F3EDE0',  /* light cream */
           200: '#E8DECA',  /* warmer cream */
           300: '#D8C9A9',  /* medium cream */
-          400: '#2D501F',  /* forest green */
-          500: '#1F3A15',  /* darker forest */
-          600: '#1A3212',  /* deep forest */
-          700: '#152A0F',  /* very dark forest */
-          800: '#0F1F0B',  /* near black forest */
-          900: '#0A1508'   /* black forest */
+          400: '#87CEEB',  /* baby blue */
+          500: '#6BA5D1',  /* medium baby blue */
+          600: '#5B8FC1',  /* darker baby blue */
+          700: '#4A7AB0',  /* deep baby blue */
+          800: '#3A659F',  /* very dark baby blue */
+          900: '#2A508E'   /* darkest baby blue */
         },
         accent: {
-          400: '#000000',  /* black */
-          500: '#000000',  /* black */
-          600: '#000000'   /* black */
+          400: '#E6E6FA',  /* light lilac */
+          500: '#D8BFD8',  /* medium lilac */
+          600: '#C8A2C8'   /* darker lilac */
         }
       }
     }
